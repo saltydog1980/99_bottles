8d4688b9ca66814dedac53c075264180efcbab06
saltydog1980 <mikeelambert99@gmail.com>
2022-05-23 22:31:38 -0400

    completed exercise

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,28 @@
-function bottleSong() {
-  // Write your code here!
-};
+// function bottleSong(n) {
+//   for (let i = n; i >= 0; i--) {
+//     if (i === 1) {
+//       console.log ("1 bottle of beer on the wall, 1 bottle of beer."
+//       +"\nTake one down and pass it around, no more bottles of beer on the wall.");
+//     } else if (i === 0) {
+//       console.log("No more bottles of beer on the wall, no more bottles of beer."
+//       +"\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+//     } else {
+//       console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.` 
+//       +`\nTake one down and pass it around, ${i-1} bottles of beer on the wall.`)
+//     }
+//   }
+// };
 
-bottleSong();
+function bottleSong(n) {
+  if (n === 1) {
+    return console.log("1 bottle of beer on the wall, 1 bottle of beer."
+    +"\nTake one down and pass it around, no more bottles of beer on the wall."
+    +"\nNo more bottles of beer on the wall, no more bottles of beer."
+    +"\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+  } else {
+    console.log(`${n} bottles of beer on the wall, ${n} bottles of beer.` 
+    +`\nTake one down and pass it around, ${n-1} bottles of beer on the wall.`)
+  }
+  bottleSong(n - 1)
+}
+
